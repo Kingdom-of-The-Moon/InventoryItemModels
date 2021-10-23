b2d7e09fe3763e5e50f78a6fb61f9ec00a47196e
omoflop <JordanBien@outlook.com>
2021-10-23 13:40:10 -0700

    Fixed gui lighting issue

--- src/main/java/org/moon/invmodels/mixin/ItemRendererMixin.java
+++ src/main/java/org/moon/invmodels/mixin/ItemRendererMixin.java
@@ -1,6 +1,8 @@
 package org.moon.invmodels.mixin;
 
-import net.minecraft.client.render.*;
+import net.minecraft.client.render.DiffuseLighting;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.ItemModels;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
@@ -17,9 +19,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Optional;
 
@@ -55,14 +55,12 @@ public class ItemRendererMixin {
 			method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V"
 	)
 	private BakedModel modifyModel(BakedModel model) {
-
 		BakedModelManagerAccess modelManager = BakedModelManagerAccess.from(this.models.getModelManager());
 		try {
 			Identifier modelID = Registry.ITEM.getId(stack.getItem());
 			if (hasCustomization(modelID)) {
 				Optional<ExtraRenderData> renderData = getCustomization(modelID, renderMode);
 				if (renderData.isPresent()) {
-					DiffuseLighting.enableGuiDepthLighting();
 					BakedModel newModel = modelManager.reallyGetModel(renderData.get().id());
 					if (newModel != null) {
 						if (renderData.get().disableLighting()) disableLighting = true;
@@ -75,20 +73,25 @@ public class ItemRendererMixin {
 		return model;
 	}
 
-	@ModifyVariable(
-			at = @At(
-					value = "INVOKE",
-					shift = At.Shift.BEFORE,
-					target = "Lnet/minecraft/client/util/math/MatrixStack;translate(DDD)V"
-			),
-			ordinal = 0,
-			method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V"
+	@Inject(
+			at = @At("HEAD"),
+			method = "renderBakedItemModel"
 	)
-	private int modifyLight(int light) {
+	private void renderBakedItemModel(BakedModel model, ItemStack stack, int light, int overlay, MatrixStack matrices, VertexConsumer vertices, CallbackInfo ci) {
 		if (disableLighting) {
-			disableLighting = false;
 			DiffuseLighting.disableGuiDepthLighting();
 		}
-		return light;
 	}
+
+	@Inject(
+			at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;enableDepthTest()V", shift = At.Shift.BEFORE),
+			method = "renderGuiItemModel"
+	)
+	private void renderGuiItemModel(ItemStack stack, int x, int y, BakedModel model, CallbackInfo ci) {
+		if (disableLighting) {
+			DiffuseLighting.enableGuiDepthLighting();
+			disableLighting = false;
+		}
+	}
+
 }
